edb2c3488f7c039a8573334c45ccd484b34125ed
PAPI131099 <58227775+PAPI131099@users.noreply.github.com>
2022-02-26 01:01:50 +0100

    Añadido nueva función para la lectura de linea, esta se tiene un limite de buffer o de memoria. Esta se incrementará en caso de que el usuario de pase el tamaño de memoria. Explicaciones en el documento README.md

--- write_inShell.c
+++ write_inShell.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv)
 //Este bucle no devuelve nada
 void lsh_loop(void){
     //Declaraciones de variables
+    //Esto estilo es como el antiguo codigo de C declarar antes 
+    //Que el resto del codigo
     char *line;
     char **args;
     int status;
@@ -30,4 +32,66 @@ void lsh_loop(void){
         free(line);
         free(args);
     }while(status);
+}
+
+//Lectura de linea
+//Definimos el espacio en memoria que usaremos para almacenar los datos
+//Buffer
+#define LSH_RL_BUFFER 1024;
+char *lsh_read_line(void){
+    //Inicializamos las variables necesarias
+    int bufsize=LSH_RL_BUFFER;
+    int position=0;
+    //Usa la liberia stdlib para usar malloc
+    //malloc( size ) asigna la memoria dada, tamaño en bloque de memoria 
+    //se usa para asignar memoria en tiempo de ejecucion
+    //sizeof() nos devuelve el tamaño de un tipo de dato char,int,etc..
+    char *buffer=malloc(sizeof(char)*bufsize);
+    int c;
+    //Verificamos si se ha asigna un valor al buffer 
+    if(!buffer){
+        //Sacamos por pantalla el error
+        //stderr es el mensaje de error estandar para imprimir por pantalla
+        fprint(stderr, "lsh: localizacion erronea. \n");
+        //finalizamos las operaciones
+        //EXIT_FAILURE finaliza el proceso por fallo.
+        exit(EXIT_FAILURE);
+    }
+
+    //Bucle aparentemente infinito
+    //Esta es la esencia de esta funcion
+    while(1){
+        //Lectura del carater
+        //Entrada de datos del caracter
+        c=getchar();
+        //Verificamos si es la final de la entrada con EOF End of File
+        //O si es igual a new line
+        //EOF es un numero entero no un caracter
+        if(c == EOF || c == '\n' ){
+            buffer[position]='\0';
+            return buffer;
+        }else{
+            buffer[position]=c;
+        }
+        position++;
+
+        //Si excede el buffer o la memoria, se recolecta
+        //Y y aumentamos el tamaño
+        if(position>=bufsize){
+            bufsize+=LSH_RL_BUFFER;
+            //realloc(ptr-puntero bloque de memoria,tamaño) intenta cambiar el tamaño del bloque de memoria
+            //al que apunta ptr, usa la libreria stdlib.h
+            buffer=realloc(buffer,bufsize);
+            //Verificamos si se ha asigna un valor al buffer 
+            if(!buffer){
+                //Sacamos por pantalla el error
+                //stderr es el mensaje de error estandar para imprimir por pantalla
+                fprint(stderr, "lsh: localizacion erronea. \n");
+                //finalizamos las operaciones
+                //EXIT_FAILURE finaliza el proceso por fallo.
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
 }
